b76163dbd4736398f260ac88d805bda95f0f47c8
Nao <abinaol@outlook.com>
2020-09-20 08:34:44 -0700

    Cookies

--- app.js
+++ app.js
@@ -35,33 +35,45 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-//authenticatin goes here
+//cookieParser signs cookies with secrete key (arg)
+app.use(cookieParser('12345-67890-09876-54321'));
+
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  //if authHeader is null
-  if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-  }
-
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0];
-  const pass = auth[1];
-
-  //check if the decoded username === 'admin' and pass === 'password'
-  if (user === 'admin' && pass === 'password') {
-      return next(); // authorized
-  } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      return next(err);
-  }
+    //check if cookie not signed
+    if (!req.signedCookies.user) {
+        const authHeader = req.headers.authorization;
+        //check if authHeader is null & challenge user to enter creds
+        if (!authHeader) {
+            const err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+        //Get username and password from the base64 encoded string
+        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        const user = auth[0];
+        const pass = auth[1];
+        if (user === 'admin' && pass === 'password') {
+            //setup set-cookie response header & sign
+            res.cookie('user', 'admin', {signed: true});
+            return next(); // authorized
+        } else {
+            const err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+    } else {
+        //check if the signCookies.user matches the username
+        if (req.signedCookies.user === 'admin') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
 }
 
 app.use(auth);

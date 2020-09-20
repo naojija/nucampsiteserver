1a042f6fc1ef01db58b7fd0d80a3f2bf95263f7b
Nao <abinaol@outlook.com>
2020-09-19 22:18:20 -0700

    Basic Authentication

--- app.js
+++ app.js
@@ -36,6 +36,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//authenticatin goes here
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  //if authHeader is null
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+
+  //check if the decoded username === 'admin' and pass === 'password'
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -63,3 +93,5 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 //create-react-express-app
+//connect to mongoDB server on one of the terminal all the time
+//mongod --dbpath=data --bind_ip 127.0.0.1
